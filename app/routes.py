2c4ad1603eead3bd83bf3e09aa7172ffea26ee2a
Luke Weaver <lwweaves@gmail.com>
2018-02-07 15:10:16 -0500

    added content, template, menu

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,19 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Luke'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Harrisburg!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'Spiderman is the ultimate super hero'
+        }
+
+    ]
+    return render_template('index.html', title="Home", user=user, posts=posts)
